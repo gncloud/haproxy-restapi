4ed8a6d6cea11e8d7af314e04050146d064c89fb
joonwoo88 <jwkim@gncloud.kr>
2017-09-01 16:00:29 +0900

    Service Test Case

--- src/test/java/io/swagger/ProxyHelperTest.java
+++ src/test/java/io/swagger/ProxyHelperTest.java
@@ -20,9 +20,40 @@ public class ProxyHelperTest {
 
         Map<String, Service> config  = new HashMap<String, Service>();
 
-
-
         //TODO service 여러개 만들어 보기..
+
+
+        Service serviceOne = new Service();
+        Service serviceTwo = new Service();
+        Service serviceThree = new Service();
+
+        // Case 1
+        serviceOne.setMode("http");
+        serviceOne.setPort(80);
+        serviceOne.setBindPort(8080);
+        serviceOne.setHost("*");
+        serviceOne.setSubdomain("www.");
+        serviceOne.setTimeout(500);
+
+        // Case 2
+        serviceTwo.setMode("tcp");
+        serviceTwo.setPort(22);
+        serviceTwo.setBindPort(2222);
+        serviceTwo.setHost("*");
+        serviceTwo.setSubdomain("tcp.");
+        serviceTwo.setTimeout(6000);
+
+        // Case 3
+        serviceThree.setMode("http");
+        serviceThree.setPort(53);
+        serviceThree.setBindPort(5553);
+        serviceThree.setHost("127.0.0.1");
+        serviceThree.setSubdomain("blog.");
+        serviceThree.setTimeout(70000);
+
+        config.put("case1", serviceOne);
+        config.put("case2", serviceTwo);
+        config.put("case3", serviceThree);
 
         ProxyHelper helper = new ProxyHelper();
         String str = helper.renderTemplate(config);
